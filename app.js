97e7130d5c3b86d8f2c906b949f4b84c0992a8fe
Ananna14 <https://github.com/Ananna14>
2021-11-30 21:35:42 +0600

    third

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
+// -----------------------toggol-navbar-------------------------------
+// ------------------------------------------------------------------
+const navToggler = document.querySelector(".nav-toggle");
+navToggler.addEventListener("click", ()=>{
+    hideSection();
+    toggleNavbar();
+    document.body.classList.toggle("hide-scrolling")
+    // console.log('hi')
+});
+function hideSection(){
+    document.querySelector("section.active").classList.toggle("fade-out")
+}
+function toggleNavbar(){
+    document.querySelector(".header").classList.toggle("active");
+}
+
+// ----------------------Active-section--------------------
+document.addEventListener("click", (e) =>{
+    if(e.target.classList.contains("link-item") && e.target.hash !== ""){
+        navToggler.classList.add("hide");
+        if(e.target.classList.contains("nav-item")){
+            toggleNavbar();
+        }
+        else{
+            hideSection();
+            document.body.classList.add("hide-scrolling")
+        }
+        setTimeout(()=>{
+            document.querySelector("section.active").classList.remove("active","fade-out");
+            document.querySelector(e.target.hash).classList.add("active");
+            window.scrollTo(0,0);
+            document.body.classList.remove("hide-scrolling");
+            navToggler.classList.remove("hide");
+        },500);
+    }
+})
+
 document.addEventListener("click", (e)=>{
     if(e.target.classList.contains("view-project-btn")){
       togglePortfolioPopup()
